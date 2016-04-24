c7bf4ca658bc162e0ab8bd093e1a077f7f68ac18
kingkong64 <kingkong2103@gmail.com>
2016-04-24 12:07:50 +0700

    start

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello Word')
+})
+
 app.set('port', (process.env.PORT || 3000))
 
 app.listen(app.get('port'), function () {
