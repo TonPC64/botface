b4bae06e1505603d8a05fedf1ef120d04f4bef11
kingkong64 <kingkong2103@gmail.com>
2016-04-24 12:32:17 +0700

    webhook

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* global messaging_events, i, event, sender, text*/
 var express = require('express')
 var app = express()
 
@@ -14,18 +13,17 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging
+  messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i]
-    sender = event.sender.id
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
     if (event.message && event.message.text) {
-      text = event.message.text
+      text = event.message.text;
       // Handle a text message from this sender
-      console.log(text)
     }
   }
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 app.set('port', (process.env.PORT || 3000))
 
