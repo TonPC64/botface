25208cb3c7bc3a85cc9ab64dfb7f97c189acf9c3
kingkong64 <kingkong2103@gmail.com>
2016-04-24 12:36:26 +0700

    send

--- index.js
+++ index.js
@@ -13,17 +13,41 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
+  messaging_events = req.body.entry[0].messaging
   for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
     if (event.message && event.message.text) {
-      text = event.message.text;
+      text = event.message.text
       // Handle a text message from this sender
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
     }
   }
-  res.sendStatus(200);
-});
+  res.sendStatus(200)
+})
+
+var token = 'CAAOZBaoxks00BAEuYKqxS0jcxyVPG88BtmZCjoxkuE9ClJHPBxlS93xmaJYXNZCYkpW2XX4U7RrIYK8YZA2wTe7ZCOl21WXenmxViZBLOZB9EYXXl1KmOBrPG01rUm44JlIkJR7t5FltgI6qaSramJH38lZB6rZB04qvI4Qw5nLb2JPmLGYPGhavZCQZCqNAArSd56S7xGmZAikFSwZDZD'
+
+function sendTextMessage (sender, text) {
+  messageData = {
+    text: text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData,
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
 
 app.set('port', (process.env.PORT || 3000))
 
