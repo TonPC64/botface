a35ca7d98d71e2c832e69f98b2813c2f70a80270
kingkong64 <kingkong2103@gmail.com>
2016-04-24 12:27:26 +0700

    webhook

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* global messaging_events, i, event, sender, text*/
 var express = require('express')
 var app = express()
 
@@ -10,6 +11,20 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong validation token')
+})
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      // Handle a text message from this sender
+      console.log(text)
+    }
+  }
+  res.sendStatus(200)
 })
 
 app.set('port', (process.env.PORT || 3000))
