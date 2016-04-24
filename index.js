60d8fab9fd4c1a930af908f7be2c4bdad98c7470
Jittaranan Tipboonde <jitinics@NICs-Jittaranans-MacBook-Air.local>
2016-04-24 15:09:49 +0700

    a

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/webhook/', function (req, res) {
 app.post('/webhook/', function (req, res) {
   var str = ''
   var messaging_events = req.body.entry[0].messaging
+  console.log(messaging_events)
   for (var i = 0; i < messaging_events.length; i++) {
     var event = req.body.entry[0].messaging[i]
     var sender = event.sender.id
