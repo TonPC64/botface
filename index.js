f1e1524159a410f85ad9370b5d02f2cbd7653d37
kingkong64 <kingkong2103@gmail.com>
2016-04-24 13:19:51 +0700

    body-parser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello Word')
