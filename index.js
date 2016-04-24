391b746778ffe87954987f8b4a3a240d1bfa3d0e
kingkong64 <kingkong2103@gmail.com>
2016-04-24 12:11:37 +0700

    verify

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello Word')
 })
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'bot_messenger_page') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong validation token')
+})
+
 app.set('port', (process.env.PORT || 3000))
 
 app.listen(app.get('port'), function () {
